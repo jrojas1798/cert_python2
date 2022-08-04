4e97db1cb0d5661c0235996da2c4b5b0d228cb40
jrojas1798 <juan.jr1278@gmail.com>
2022-08-04 13:01:21 -0500

    laboratorios Parte 2

---
+++ Modulo3/lab3.4.1.12.py
@@ -0,0 +1,21 @@
+#Laboratorio 3.4.1.12
+
+class Timer:
+    def __init__(timer):        
+        return
+
+    def __str__(self):
+        return
+
+    def next_second(self):
+        return
+
+    def prev_second(self):
+        return
+
+timer = Timer(23, 59, 59)
+print(timer)
+timer.next_second()
+print(timer)
+timer.prev_second()
+print(timer)
